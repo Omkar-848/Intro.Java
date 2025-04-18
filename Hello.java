ab2926814dd393579f4f4ce9b44d895a51af4eda
OMKAR CHAVAN <ovchavan3456@gmail.com>
2025-04-18 10:02:54 -0700

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+package introjava;
+
+public class Hello {
+            public static void main(String[] args) {
+	        System.out.println("Hello, World omkar!");  // Prints Hello World to the console
+	    }
+	}
+
+
